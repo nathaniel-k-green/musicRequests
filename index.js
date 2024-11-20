6e60a3b6b7588170a1c5b8e362cf5eed3b163ada
nathaniel-k-green <nagreen778@gmail.com>
2024-11-20 00:29:53 -0600

    added code

---
+++ index.js
@@ -0,0 +1,18 @@
+async function addSong() {
+    const song = document.getElementById("song").value;
+    const token = "abc123xyz";  // Replace with dynamic token in real implementation
+
+    if (song) {
+        const response = await fetch(`https://e5jhnqnkm6.execute-api.us-east-2.amazonaws.com/prod/musicRequests/add-song?token=${token}&song=${song}`, {
+            method: 'POST',
+            headers: {
+                'x-api-key': '${{ secrets.ADD_SONG_API_KEY }}',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ song: song })
+        });
+
+        const data = await response.json();
+        alert(data.message);
+    }
+}
