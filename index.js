c51a4ba52973c4168e517eecec935117edd6e77b
nathaniel-k-green <nagreen778@gmail.com>
2024-11-20 00:44:12 -0600

    added code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ async function addSong() {
     const token = "abc123xyz";  // Replace with dynamic token in real implementation
 
     if (song) {
-        const response = await fetch(`https://e5jhnqnkm6.execute-api.us-east-2.amazonaws.com/prod/musicRequests/add-song?token=${token}&song=${song}`, {
+        const response = await fetch(`https://e5jhnqnkm6.execute-api.us-east-2.amazonaws.com/prod/musicRequests/add-song`, {
             method: 'POST',
             headers: {
                 'x-api-key': '${{ secrets.ADD_SONG_API_KEY }}',
